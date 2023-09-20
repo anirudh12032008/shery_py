dcdee2f599630a0066e23a558e126e6695798854
Anirudh sahu <renudilip19@gmail.com>
2023-09-20 19:27:35 +0530

    20sep

---
+++ 20sepMM.py
@@ -0,0 +1,23 @@
+# Functions
+# 1. Making Function body 
+# def func_name():
+    # function body/scope
+
+# 2.Calling function 
+# func_name()
+
+# Types 
+# non-parametrized functions  ---- no param
+# parametrized functions   ----- params are there
+
+# def greet(param):  here param === functional parameter
+    # print(param)
+
+# greet(x) here x === functional arguments
+# print("Hello")
+
+# return type and non-return type 
+# every func returns None if no value is returned
+# def a():
+    # return 1
+# print(a())
