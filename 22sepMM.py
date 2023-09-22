a4502a80a0ef3777b274683a8da40c2e0d59925f
Anirudh sahu <renudilip19@gmail.com>
2023-09-22 19:23:52 +0530

    22 sep

---
+++ 22sepMM.py
@@ -0,0 +1,23 @@
+# keyword arguments
+# def num(a,b,c,d):
+    # print(a,b,c,d)
+# num(1,6,d=5,c=34) ---> a = 1, b = 6 , c = 34, d = 5
+
+# default parameters
+# def sum(a,b=4): ----> b is having default value 
+    # print(a,b)
+# default param is always the last parameter
+
+# lambda function (it is a expression)
+# def sum(a,b):       -------Statement Function
+    # return a + b    -------
+
+# sum = lambda a,b:a+b 
+
+
+# if else expression
+# global keyword in function 
+# function inside function 
+
+
+# string ifelse loop function 
